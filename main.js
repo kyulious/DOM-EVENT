3298e8dec59bf448fc3fd8ef8cf82bdf84955611
kyulious <kyulikim1@gmail.com>
2021-02-27 19:08:42 +0900

    add comment

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var result = "";
 for(var i=0; i<200; i++){
     result += '<img src="img/pic'+i+'.jpg">';
 }
+
+// HTML 요소에 접근해 내용을 바꾸는 것.
 frame.innerHTML=result;
 
 var imgs=document.getElementsByTagName("img");
@@ -12,9 +14,9 @@ console.log(imgs);
 
 //브라우저에서 마우스 움직일 때 
 window.onmousemove = function(e){
-    var x = e.pageX;
-    var total = window.innerWidth;
-    var percent = parseInt((x/total)*num);
+    var x = e.pageX; //문서 왼쪽 상단을 기준으로 마우스 위치의 X좌표 값
+    var total = window.innerWidth; //window.innerWidth: 창틀을 뺀 안 쪽 전체 가로길이
+    var percent = parseInt((x/total)*num); //parseInt : 정수로 바꿔줌
     console.log(percent);
     //마우스 움직일 때 순간적으로 모든 이미지값을 비활성화 시켜 초기화
     for(var i=0; i<num; i++){
